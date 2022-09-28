3d841874d25d45ef96efcdeef8e4d0bc7e10453f
justin <fezzygo@gmail.com>
2022-09-27 22:59:37 -0400

    removed boostrap import from index file (src/index.js:5)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 // React v18
 const container = document.getElementById('root');
