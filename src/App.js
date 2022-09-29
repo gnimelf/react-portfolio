8a57045d19ee1a51a3c1abd3e39540512db6ca74
justin <fezzygo@gmail.com>
2022-09-28 23:59:36 -0400

    added font awesome icons to the site (src/App.js:3,4,7)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './reset.css'
 import './style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
-
+library.add( fab )
 const App = () => <PortfolioContainer />;
-
 
 export default App;
