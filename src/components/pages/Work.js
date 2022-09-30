633169698c39fbd44baac7fe4071bf53a0b992e2
justin <fezzygo@gmail.com>
2022-09-30 08:29:50 -0400

    updated rel  property  on all <a> tags to fix react security warning(src/components/pages/Work.js:13,23,33,43,53,62,71,80,89,98,107,116,125,134)

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -4,144 +4,142 @@ export default function Work() {
     return (
         <main>
             <section id="work">
-                <h1 class="sub-heading">Work</h1>
-                <section class="projects">
-                    <figure class="main-img">
-                        <p class="tag">MVC</p>
+                <h1 className="sub-heading">Work</h1>
+                <section className="projects">
+                    <figure className="main-img">
+                        <p className="tag">MVC</p>
                         <a
                             href="https://shopping-list-jf.herokuapp.com/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="hero-project"></section>
+                            <section className="hero-project"></section>
                         </a>
                     </figure>
                     {/* TECH BLOG */}
-                    <figure class="link-img">
-                        <p class="tag">MVC</p>
+                    <figure className="link-img">
+                        <p className="tag">MVC</p>
                         <a
                             href="https://tech-blog-jf.herokuapp.com/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj2"></section>
+                            <section className="sub-project sub-proj2"></section>
                         </a>
                     </figure>
                     {/* PROJECT 2 */}
-                    <figure class="link-img">
-                        <p class="tag">MVC Group Project 2</p>
+                    <figure className="link-img">
+                        <p className="tag">MVC Group Project 2</p>
                         <a
                             href="https://shopping-list-project2.herokuapp.com"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj3"></section>
+                            <section className="sub-project sub-proj3"></section>
                         </a>
                     </figure>
                     {/* PASS GEN */}
-                    <figure class="link-img">
-                        <p class="tag">HTML + CSS + JS</p>
+                    <figure className="link-img">
+                        <p className="tag">HTML + CSS + JS</p>
                         <a
                             href="https://gnimelf.github.io/Password-Generator/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj4"></section>
+                            <section className="sub-project sub-proj4"></section>
                         </a>
                     </figure>
                     {/* CODE QUIZ */}
-                    <figure class="link-img">
-                        <p class="tag">HTML + CSS + JS</p>
+                    <figure className="link-img">
+                        <p className="tag">HTML + CSS + JS</p>
                         <a
                             href="https://gnimelf.github.io/js-code-quiz-app/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj5"></section>
+                            <section className="sub-project sub-proj5"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">Node + MySQL</p>
+                    <figure className="link-img">
+                        <p className="tag">Node + MySQL</p>
                         <a
                             href="https://github.com/gnimelf/Employee-Tracker"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj6"></section>
+                            <section className="sub-project sub-proj6"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">Node</p>
+                    <figure className="link-img">
+                        <p className="tag">Node</p>
                         <a
                             href="https://github.com/gnimelf/team-profile-generator"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj7"></section>
+                            <section className="sub-project sub-proj7"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">Server Side API's</p>
+                    <figure className="link-img">
+                        <p className="tag">Server Side API's</p>
                         <a
                             href="https://gnimelf.github.io/weather-dashboard/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj8"></section>
+                            <section className="sub-project sub-proj8"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">WebAPI's</p>
+                    <figure className="link-img">
+                        <p className="tag">WebAPI's</p>
                         <a
                             href="https://gnimelf.github.io/Work-Day-Scheduler/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj9"></section>
+                            <section className="sub-project sub-proj9"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">WebAPI's</p>
+                    <figure className="link-img">
+                        <p className="tag">WebAPI's</p>
                         <a
                             href="https://gnimelf.github.io/Project-Tracker/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj10"></section>
+                            <section className="sub-project sub-proj10"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">Server Side API's</p>
+                    <figure className="link-img">
+                        <p className="tag">Server Side API's</p>
                         <a
                             href="https://gnimelf.github.io/Library-of-Congress-Search-Tool/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj11"></section>
+                            <section className="sub-project sub-proj11"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">WebAPI's</p>
+                    <figure className="link-img">
+                        <p className="tag">WebAPI's</p>
                         <a
                             href="https://gnimelf.github.io/Word-Guess/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj12"></section>
+                            <section className="sub-project sub-proj12"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">Group Project 1</p>
+                    <figure className="link-img">
+                        <p className="tag">Group Project 1</p>
                         <a
                             href="https://gnimelf.github.io/project-1/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj13"></section>
+                            <section className="sub-project sub-proj13"></section>
                         </a>
                     </figure>
-                    <figure class="link-img">
-                        <p class="tag">HTML Refactor</p>
+                    <figure className="link-img">
+                        <p className="tag">HTML Refactor</p>
                         <a
                             href="https://gnimelf.github.io/HTML-CSS-and-Git-Code-Refactor/"
-                            target="_blank"
+                            target="_blank" rel="noopener noreferrer"
                         >
-                            <section class="sub-project sub-proj14"></section>
+                            <section className="sub-project sub-proj14"></section>
                         </a>
                     </figure>
 
-                    <figure class="link-img">
-                        <p class="tag">Coming Soon</p>
-                        <a href="#">
-                            <section class="sub-project"></section>
-                        </a>
+                    <figure className="link-img">
+                        <p className="tag">Coming Soon</p>
+                            <section className="sub-project"></section>
                     </figure>
                 </section>
             </section>
