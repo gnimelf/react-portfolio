538a21321cdf1e4d4b9c69d87717ef3c0ebf0d0e
justin <fezzygo@gmail.com>
2022-09-30 08:25:26 -0400

    update contact page to validate the input values before allowing a submit:(/src/components/pages/Contact.js:1-50, 60,64,68,70,73,80)

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Contact() {
+// Form validators
+import { validateEmail } from "../../utils/helpers";
+
+function Contact() {
+    // Setup state variables
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    // Handle changes to input fields
+    const handleInputChange = (e) => {
+
+        console.log(e.target.value);
+
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        // Set state for first name, last name, and email
+        if (inputType === "firstName") {
+            setFirstName(inputValue);
+        } else if (inputType === "lastName") {
+            setLastName(inputValue);
+        } else if (inputType === "email"){
+            setEmail(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        console.log(e.target)
+        e.preventDefault();
+
+        if (!firstName || !lastName) {
+            setErrorMessage("First Name or Last Name is invalid");
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            setErrorMessage("email address is invalid");
+            return;
+        }
+
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setErrorMessage("")
+    };
+
+    // return form
     return (
         <main>
-            <section id="about-me">
-            <h1 className="sub-heading">Contact me</h1>
-            <p>
-            <form action="mailto:fezzygo@gmail.com">
-                <label for='fname'>First Name</label><br/>
-                <input name='fname' required='true'/><br/>
-                <label for='lname'>Last Name</label ><br/>
-                <input name='lname' required='true'/><br/>
-                <label for='email' >Email</label><br/>
-                <input name="email" required='true'/><br/>
-                <button type="sumbit">Send</button>
-            </form>
-            </p>
-        </section>
-        
-
+            <section id="contact-me">
+                <h1 className="sub-heading">Contact me</h1>
+                <form>
+                    <label>First Name:</label>
+                    <br />
+                    <input name="firstName" onChange={handleInputChange} />
+                    <br />
+                    <label>Last Name:</label>
+                    <br />
+                    <input name="lastName" onChange={handleInputChange} />
+                    <br />
+                    <label>Email:</label>
+                    <br />
+                    <input name="email" onChange={handleInputChange} />
+                    <br />
+                    <button type="button" onClick={handleFormSubmit}>
+                        Send
+                    </button>
+                    <p className="error-text">{errorMessage}</p>
+                </form>
+            </section>
         </main>
     );
 }
+
+export default Contact;
