58e331e89f8d9aa9b3af1b9b8879448f9a446006
Justin Fleming <gnimelf@users.noreply.github.com>
2024-04-24 23:38:04 -0400

    added dynamic date to footer (src/components/Footer.js:6)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer () {
     return (
         <footer>
-            <h2>&copy; 2022 Justin Fleming</h2>
+            <h2>&copy; {new Date().getFullYear()} Justin Fleming</h2>
         </footer>
     )
 }
